d51f7bca340aac2f351800068c8fb23eb53e7bf6
luatoolz <luatoolz@somahorse.com>
2024-09-27 18:36:35 +0300

    rm: init.d (testdata ^ t.definer) load

--- lua/t/init.d/td_def.lua
+++
@@ -1,7 +0,0 @@
-local meta = assert(require "meta", "no: meta")
-local t = assert(t or require "t", "t.def:no: t")
-local mod = meta.module('testdata')
-if mod.exists then
-  local td = assert(require "testdata", "t.def:no: testdata")
-  return require("t.storage.mongo") ^ td.def
-end
